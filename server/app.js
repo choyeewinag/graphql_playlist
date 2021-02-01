2e07f5ea42bedcb231d870d04f9cd73b347fde6b
Cho Yee Win Aung <choyeewinag>
2021-02-01 14:24:43 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const{ graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+//one supercharged endpoint
+
+app.listen(3000, () => {
+    console.log('Now listening for request on port 3000.');
+})
