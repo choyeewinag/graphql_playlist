82710e8f885067026eeacb48fc1d10158910ba00
Cho Yee Win Aung <choyeewinag>
2021-02-01 18:38:04 -0500

    schema.js file

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const{ graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
-//one supercharged endpoint
+// bind express with graphql
 app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 
-}))
-
-app.listen(3000, () => {
-    console.log('Now listening for request on port 3000.');
-})
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,41 +1,38 @@
-//create a schema file
-
 const graphql = require('graphql');
-const { resolve } = require('path');
+const _ = require('lodash');
 
-//destructing aka grabbing the variable/function from the graphql object
-const { GraphQlObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
-//define the first object type of the graph and set it to the variable called "BookType"
-const BookType = new GraphQlObjectType({ //reusing line 4
-    name: 'Book', //name of the object
-    fields: ()=>({//fields should be functions //id, name and genre are all fields
-        id: { type: GraphQLString}, //define the type such as string or int, but can't just say "string" should grab from the graphQL object refer line 4
-        name: {type: GraphQLString},
-        genre: {type: GraphQLString}
-    }), 
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: ( ) => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
 });
 
-//create a relationship between the types if we have more than one. But in this case, we only have "BookType"
-
-//create a root query
-const RootQuery = new GraphQlObjectType({
+const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
-    fields: () => ({
+    fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString}}, //for user to pass the argument along when they request. For example, what's the idea of the book?
-             /* From front end, it looks like this 
-             book(id:123){ //id is argument aka args
-                 name
-                 genre
-             }
-             */
-            resolve(parent, args){ //parents comes into play when we have relational data
-                //code to get data from db/other source
-                args.id //grab that book with the id from args
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args){
+                // code to get data from db / other source
+                return _.find(books, { id: args.id });
             }
         }
-    })
-})
+    }
+});
 
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
